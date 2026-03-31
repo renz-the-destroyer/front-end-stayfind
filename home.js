87aeb38168e1a4bf301be89c780e37cf7d853034
Stay Find <renzv231@gmail.com>
2026-03-31 14:09:24 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 // REPLACE THIS with your Render URL
-const API_BASE = "https://your-render-app-name.onrender.com/api";
+const API_BASE = "https://stayfind-app.onrender.com/api";
 
 const currentUser = JSON.parse(localStorage.getItem('user'));
 const listingsGrid = document.getElementById('listingsGrid');
