970b04c08cb74f29b20cf3b4fe4da0fed27af50b
Stay Find <renzv231@gmail.com>
2026-03-31 14:30:24 +0800

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,5 @@
 // REPLACE THIS with your Render URL
-const API_BASE = "https://stayfind-app.onrender.com/api";
+const API_BASE = "https://stayfind-app-system.onrender.com/api";
 
 let userRole = null;
 let currentUser = JSON.parse(localStorage.getItem('user'));
